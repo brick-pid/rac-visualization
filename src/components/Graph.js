ca4ae753c8555827ba0e800f4fd4b80108d576a4
brick-pid <winphoneljb@live.com>
2023-03-16 18:44:59 +0800

    加入Tooltip组件，实现悬浮显示节点信息功能

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,13 +1,31 @@
 import Graphin, { Behaviors } from "@antv/graphin"
 import graphStore from "../stores/GraphStore"
 import { observer } from "mobx-react-lite"
+import { Components } from "@antv/graphin"
+const { Tooltip } = Components
+
 
 const { ZoomCanvas } = Behaviors
 
 function Graph () {
+  const handleTooltip = (value) => {
+    if (value && value.model) {
+      const { model } = value
+      return (
+        <div>
+          <p>ID: {model.id}</p>
+          <p>Label: {model.style.keyshape.size}</p>
+        </div>
+      )
+    }
+    return null
+  }
   return (
     <Graphin data={graphStore.graphData} >
       <ZoomCanvas disabled />
+      <Tooltip bindType="node" style={{ padding: '10px' }}>
+        {handleTooltip}
+      </Tooltip>
     </Graphin>
   )
 }
--- src/stores/GraphStore.js
+++ src/stores/GraphStore.js
@@ -1,29 +1,55 @@
 import { makeAutoObservable } from "mobx"
 
+const data = {
+  "istio-ingressgateway": {
+    "score": 1128.8893690579082,
+    "pattern": ["istio-ingressgateway"],
+    "pattern_score": 0.5017286084701814,
+    "in-pattern_score": 2250
+  },
+  "ts-ui-dashboard": {
+    "score": 32.84087411511234,
+    "pattern": ["ts-ui-dashboard"],
+    "pattern_score": 0.33856571252693135,
+    "in-pattern_score": 97
+  },
+  "ts-admin-order-service": {
+    "score": 366.953385127636,
+    "pattern": ["ts-admin-order-service"],
+    "pattern_score": 0.3190899001109878,
+    "in-pattern_score": 1150
+  }
+}
+
+const nodes = Object.keys(data).map(key => ({
+  id: key,
+  style: {
+    keyshape: {
+      size: Math.sqrt(data[key].score * 10), // 将 Score 归一化为节点大小
+    },
+    label: {
+      value: key,
+    }
+  }
+}))
+
+const edges = []
+
+const graphDataMooc = {
+  nodes,
+  edges,
+}
+
+
+console.log(graphDataMooc)
+
 class GraphStore {
   constructor() {
     makeAutoObservable(this)
   }
 
   // Observable state
-  graphData = {
-    nodes: [
-      {
-        id: "node1",
-        label: "node1",
-      },
-      {
-        id: "node2",
-        label: "node2",
-      },
-    ],
-    edges: [
-      {
-        source: "node1",
-        target: "node2",
-      },
-    ],
-  }
+  graphData = graphDataMooc
 
   // Actions
   setGraphData = (graphData) => {
