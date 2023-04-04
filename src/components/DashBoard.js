7fafbf288bdc5086f68b443453af26c93559aa9f
brick-pid <winphoneljb@live.com>
2023-04-04 08:47:08 +0800

    update the design of menu

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -7,37 +7,75 @@ import Setting from './Setting'
 import LoginPage from './Login'
 import { BrowserRouter, Route, Routes, NavLink } from 'react-router-dom'
 
-const { Header } = Layout
-
+const { Header, Content, Sider } = Layout
+const { SubMenu } = Menu
 
 function Dashboard () {
   return (
     <BrowserRouter>
       <Layout>
-        <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
-          <div className="logo" />
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1">
-              <NavLink to='/graph' activeClassName="active"> Graph </NavLink>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <NavLink to='/rank' activeClassName="active"> Rank </NavLink>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <NavLink to='/microservices' activeClassName="active"> Microservices </NavLink>
-            </Menu.Item>
-            <Menu.Item key="4">
-              <NavLink to='/setting' activeClassName="active"> Setting </NavLink>
-            </Menu.Item>
-          </Menu>
+        <Header style={{ position: 'fixed', zIndex: 1, width: '100%', backgroundColor: 'white' }}>
+          <div className="logo" style={{ fontWeight: 'bold', fontSize: '2rem' }}>
+            A system for detecting suspicious microservices
+          </div>
         </Header>
-        <Routes>
-          <Route path="/graph" element={<Header><Graph /></Header>} />
-          <Route path="/rank" element={<Header><Rank /></Header>} />
-          <Route path="/microservices" element={<Header><MicroservicesPage /></Header>} />
-          <Route path="/setting" element={<Header><Setting /></Header>} />
-          <Route path="login" element={<Header><LoginPage /></Header>} />
-        </Routes>
+        <Layout style={{ marginTop: '64px' }}>
+          <Sider width={200} className="site-layout-background">
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['sub1']}
+              style={{ height: '100%', borderRight: 0 }}
+            >
+              <SubMenu key="sub1" title="Graph">
+                <Menu.Item key="1">
+                  <NavLink to="/graph" activeClassName="active">
+                    Graph
+                  </NavLink>
+                </Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub2" title="Rank">
+                <Menu.Item key="2">
+                  <NavLink to="/rank" activeClassName="active">
+                    Rank
+                  </NavLink>
+                </Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub3" title="Microservices">
+                <Menu.Item key="3">
+                  <NavLink to="/microservices" activeClassName="active">
+                    Microservices
+                  </NavLink>
+                </Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub4" title="Setting">
+                <Menu.Item key="4">
+                  <NavLink to="/setting" activeClassName="active">
+                    Setting
+                  </NavLink>
+                </Menu.Item>
+              </SubMenu>
+            </Menu>
+          </Sider>
+          <Layout style={{ padding: '0 24px 24px' }}>
+            <Content
+              className="site-layout-background"
+              style={{
+                padding: 24,
+                margin: 0,
+                minHeight: 280,
+              }}
+            >
+              <Routes>
+                <Route path="/graph" element={<Graph />} />
+                <Route path="/rank" element={<Rank />} />
+                <Route path="/microservices" element={<MicroservicesPage />} />
+                <Route path="/setting" element={<Setting />} />
+                <Route path="login" element={<LoginPage />} />
+              </Routes>
+            </Content>
+          </Layout>
+        </Layout>
       </Layout>
     </BrowserRouter>
   )
