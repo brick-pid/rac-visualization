b1482b80c9b4e7d5ee567d4dbc32d013ad89f881
brick-pid <winphoneljb@live.com>
2023-05-02 11:47:49 +0800

    更新使用graphStore的地方

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -2,17 +2,31 @@ import Graphin, { Behaviors } from "@antv/graphin"
 import graphStore from "../stores/GraphStore"
 import { observer } from "mobx-react-lite"
 import { Components } from "@antv/graphin"
-import { Button } from "antd"
+import { Button, DatePicker } from "antd"
 import LayoutSelection from "./LayoutSelection"
+import { useState } from "react"
 
 const { Tooltip } = Components
-
+const { RangePicker } = DatePicker
 
 const { ActivateRelations } = Behaviors
 
 
 
 function Graph () {
+  const [dateRange, setDateRange] = useState([])
+
+  const handleDateRangeChange = (dates) => {
+    setDateRange(dates)
+  }
+
+  const getMonthRange = (offset) => {
+    const date = new Date()
+    const firstDay = new Date(date.getFullYear(), date.getMonth() - offset, 1)
+    const lastDay = new Date(date.getFullYear(), date.getMonth() + 1 - offset, 0)
+    return [firstDay, lastDay]
+  }
+
   const handleTooltip = (value) => {
     if (value && value.model) {
       const { model } = value
@@ -31,8 +45,23 @@ function Graph () {
 
   return (
     <div>
-      <LayoutSelection />
-      <Button onClick={() => { }} style={{ marginLeft: '10px' }}> test button</Button>
+      <div>
+        <LayoutSelection />
+        <Button onClick={() => { }} style={{ marginLeft: '10px' }}> 故障传播路径 </Button>
+        <RangePicker
+          style={{ marginLeft: '10px' }}
+          value={dateRange}
+          presets={{
+            'Last Month': getMonthRange(1),
+            'Last 3 Months': getMonthRange(3),
+            'Last 6 Months': getMonthRange(6),
+          }}
+          onChange={handleDateRangeChange}
+        />
+      </div>
+
+
+
 
       {graphStore.isEdgesDataLoaded && (
         <Graphin
