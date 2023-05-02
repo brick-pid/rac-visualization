3130aaf76a550ccec0d073891da6cec6895ee888
brick-pid <winphoneljb@live.com>
2023-05-02 11:34:55 +0800

    update graphstore to match the backend api

--- src/stores/GraphStore.js
+++ src/stores/GraphStore.js
@@ -1,39 +1,6 @@
 import { makeAutoObservable, action } from "mobx"
 import axios from "axios"
 
-const services = ['ts-cancel-service', 'ts-travel2-service', 'ts-train-service', 'ts-notification-service', 'ts-order-other-service', 'ts-payment-service', 'ts-admin-basic-info-service', 'ts-consign-service', 'ts-preserve-service', 'ts-order-service', 'ts-inside-payment-service', 'ts-config-service', 'ts-price-service', 'ts-seat-service', 'ts-travel-plan-service', 'ts-route-plan-service', 'ts-ticketinfo-service', 'ts-station-service', 'ts-contacts-service', 'ts-preserve-other-service', 'ts-route-service', 'ts-security-service', 'ts-travel-service', 'ts-admin-route-service', 'ts-basic-service', 'istio-ingressgateway', 'ts-ui-dashboard', 'ts-admin-order-service', 'ts-auth-service', 'ts-food-map-service', 'ts-assurance-service', 'ts-food-service', 'ts-user-service', 'ts-verification-code-service', 'ts-admin-user-service', 'ts-admin-travel-service']
-const scores = { 'istio-ingressgateway': 1128.8893690579082, 'ts-ui-dashboard': 32.84087411511234, 'ts-admin-order-service': 366.953385127636 }
-const total_invocations = 1526
-
-// add a score field to each service if the scores less than 1/3 of total_invocations, then use 1/3 of total_invocations
-for (let i = 0; i < services.length; i++) {
-  if (!scores[services[i]] || scores[services[i]] < total_invocations / 3) {
-    scores[services[i]] = total_invocations / 3
-  }
-}
-
-// if a service has a score in scores, then use it, otherwise use 1
-const service_data = services.map((service) => ({
-  id: service,
-  score: scores[service] || 0,
-  prob: scores[service] / total_invocations
-}))
-
-// console.log(service_data)
-
-const nodes = service_data.map(service => ({
-  id: service.id,
-  style: {
-    keyshape: {
-      size: service.prob * 100,
-    },
-    label: {
-      value: service.id,
-    }
-  }
-}))
-
-
 // add a method to get critical path which will be highlighted visually
 const criticalPath = [
   {
@@ -74,20 +41,24 @@ class GraphStore {
 
   // graph data
   graphData = {
-    nodes: nodes,
+    nodes: [],
     edges: []
   };
 
-  // 添加一个状态，表示边数据是否已加载
+  // 添加一个状态，表示数据是否已加载
   isEdgesDataLoaded = false;
+  isNodesDataLoaded = false;
 
   setGraphData = (graphData) => {
     this.graphData = graphData
   };
 
-  // 更新edges数据
+  // 更新数据
   setEdgesData = (edgesData) => {
     this.graphData.edges = edgesData
+  };
+  setNodesData = (nodesData) => {
+    this.graphData.nodes = nodesData
   };
 
   // layout
@@ -103,26 +74,49 @@ const graphStore = new GraphStore()
 // 通过 action 处理异步请求
 const fetchEdgesData = action(async () => {
   try {
-    const response = await axios.get("http://localhost:8080/invocation/edges")
-    const edges_data = response.data.map((edge) => ({
-      source: edge.source,
-      target: edge.target,
-      style: {
-        keyshape: {
-          lineWidth: edge.probability * 120,
-        },
-      },
-    }))
+    const response = await axios.get("http://localhost:5000/invocation/random")
+    const edges_data = response.data
     // 请求成功后，调用`setEdgesData`方法更新`edges_data`，并将`isEdgesDataLoaded`设置为`true`
     graphStore.setEdgesData(edges_data)
     graphStore.isEdgesDataLoaded = true
-    console.log("graphStore 中请求 edges 数据完成", graphStore.graphData)
+    console.log("graphStore 中请求 edges 数据完成", graphStore.graphData.edges)
   } catch (error) {
-    console.error("error", error)
+    console.error("fetch edges data error", error)
   }
 })
 
 // 在graphStore类外部调用`fetchEdgesData`执行异步请求
 fetchEdgesData()
 
+// 通过 action 处理异步请求
+const fetchNodesData = action(async () => {
+  try {
+    const response = await axios.get("http://localhost:5000/microservice/all")
+    const nodes_data = response.data
+    // 请求成功后，调用`setNodesData`方法更新`nodes_data`，并将`isNodesDataLoaded`设置为`true`
+    graphStore.setNodesData(nodes_data)
+    graphStore.isNodesDataLoaded = true
+    console.log("graphStore 中请求 nodes 数据完成", graphStore.graphData.nodes)
+  } catch (error) {
+    console.error("fetch nodes data error", error)
+  }
+})
+
+// 在graphStore类外部调用`fetchNodesData`执行异步请求
+fetchNodesData()
+
 export default graphStore
+
+
+// graphin 的数据格式
+// {
+//   id: node.name,
+//   style: {
+//     keyshape: {
+//       size: node.probability * 100,
+//     },
+//     label: {
+//       value: node.name,
+//     }
+//   }
+// }
